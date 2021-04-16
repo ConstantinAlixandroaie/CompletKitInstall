48b912d3016f24809108437e3beb3d1a10d9242d
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2021-04-16 18:25:22 +0300

    bootsrap 5.0 installed Home page carousel Updated

--- CompletKitInstall/Migrations/20210414132234_initial2.Designer.cs
+++ CompletKitInstall/Migrations/20210414132234_initial2.Designer.cs
@@ -4,14 +4,16 @@ using CompletKitInstall.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CompletKitInstall.Migrations
 {
     [DbContext(typeof(CompletKitDbContext))]
-    partial class CompletKitDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210414132234_initial2")]
+    partial class initial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CompletKitInstall/Migrations/20210414132234_initial2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompletKitInstall.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateCreated",
+                table: "Products",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateCreated",
+                table: "Categories",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Categories");
+        }
+    }
+}
