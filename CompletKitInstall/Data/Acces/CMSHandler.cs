afedbe1335633953d6798742062464cecd8c0843
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2021-06-13 18:30:09 +0300

    Updates in Cms Handler

--- CompletKitInstall/Data/Acces/CMSHandler.cs
+++ CompletKitInstall/Data/Acces/CMSHandler.cs
@@ -1,24 +1,36 @@
-﻿using System;
+﻿using CompletKitInstall.Models;
+using CompletKitInstall.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CompletKitInstall.Data.Acces
 {
 
-    public interface ICMSHandler 
+    public interface ICMSHandler
     {
         public Task AddCarouselContent();
         public Task GetCarouselContent();
         public Task EditCarouselContent();
         public Task RemoveCarouselContent();
         public Task AddCardContent();
-        public Task GetCardContent();
-        public Task EditCardContent();
+        public Task<IEnumerable<CardContentViewModel>> GetCardContent();
+        public Task<bool> EditCardContent(int id, CardContentViewModel newData,ClaimsPrincipal user);
         public Task RemoveCardContent();
     }
     public class CMSHandler : ICMSHandler
     {
+        protected CompletKitDbContext _ctx;
+        protected readonly IAuthorizationService _authorizationService;
+        public CMSHandler(CompletKitDbContext ctx, IAuthorizationService authorizationService)
+        {
+            _ctx = ctx;
+            _authorizationService = authorizationService;
+        }
         public Task AddCardContent()
 
         {
@@ -30,19 +42,57 @@ namespace CompletKitInstall.Data.Acces
             throw new NotImplementedException();
         }
 
-        public Task EditCardContent()
+        public async Task<bool> EditCardContent(int id,CardContentViewModel newData,ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var cardContent = await _ctx.CardContents.FirstOrDefaultAsync(x => x.Id == id);
+                if (cardContent == null)
+                    return false;
+                if ((newData.CardText != null) && (string.Equals(newData.CardText, cardContent.CardText)))
+                    cardContent.CardText = newData.CardText;
+                if ((newData.CardFooter != null) && (string.Equals(newData.CardFooter, cardContent.CardFooter)))
+                    cardContent.CardFooter = newData.CardFooter;
+                if ((newData.ImageUrl != newData.ImageUrl) && (string.Equals(newData.ImageUrl, cardContent.ImageUrl)))
+                    cardContent.ImageUrl = newData.ImageUrl;
+                await _ctx.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
         }
 
         public Task EditCarouselContent()
         {
             throw new NotImplementedException();
         }
-
-        public Task GetCardContent()
+        public  async Task<IEnumerable<CardContentViewModel>> GetCardContent()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var rv = new List<CardContentViewModel>();
+                var sourceCollection = await _ctx.CardContents.ToListAsync();
+                foreach (var cardContent in sourceCollection)
+                {
+                    var vm = new CardContentViewModel()
+                    {
+                        Id = cardContent.Id,
+                        CardText = cardContent.CardText,
+                        CardFooter = cardContent.CardFooter,
+                        ImageUrl = cardContent.ImageUrl
+                    };
+                    rv.Add(vm);
+                }
+                return rv;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
         }
 
         public Task GetCarouselContent()
---
+++ CompletKitInstall/ViewModels/CardContentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompletKitInstall.ViewModels
+{
+    public class CardContentViewModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string CardText { get; set; }
+        public string CardFooter { get; set; }
+    }
+}
