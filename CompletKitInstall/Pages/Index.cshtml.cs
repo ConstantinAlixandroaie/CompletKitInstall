acda90cb71ae4043ca17c697ef1f39ef56bec014
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2021-07-24 15:48:06 +0300

    Release profiles added. Picture added. Prerelease Build almost done.

--- CompletKitInstall/Pages/Index.cshtml.cs
+++ CompletKitInstall/Pages/Index.cshtml.cs
@@ -6,14 +6,33 @@ using CompletKitInstall.Authorization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
 
 namespace CompletKitInstall.Pages
 {
+    
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        [BindProperty]
+        public bool UserIsSignedIn { get; set; }
+        public IndexModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
         public void OnGet()
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                UserIsSignedIn = true;
+            }
+            else
+            {
+                UserIsSignedIn = false;
+            }
         }
     }
 }
