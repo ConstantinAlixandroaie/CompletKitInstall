b354476c4b125b49a7a7f0b6c04b6d3739b4848d
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2021-08-29 13:53:33 +0300

    Gui Updates

--- CompletKitInstall/wwwroot/js/AddProduct.js
+++ CompletKitInstall/wwwroot/js/AddProduct.js
@@ -1,4 +1,4 @@
-﻿
+﻿//preview catalog images
 function preview_image() {
     total_file = document.getElementById("ctl_images").files.length;
     for (var i = 0; i < total_file; i++) {
@@ -8,6 +8,19 @@ function preview_image() {
         $('.btn-close').click(function () {
             $(this).parent(".pip").remove();
             $('#previmg' + i).click(function () { (this).remove(); });
+        });
+    }
+}
+//preview product image
+function preview_prodimage() {
+    total_file = document.getElementById("prod_image").files.length;
+    for (var i = 0; i < total_file; i++) {
+        $('#prodimage_preview').append("<span class=\"pip\">" +
+            "<img class='img-preview' id='prodimg" + i + "'src='" + URL.createObjectURL(event.target.files[i]) + "'>"
+            + "<br/><button class=\"btn-close\" aria-label='Close'></button>" + "</span>");
+        $('.btn-close').click(function () {
+            $(this).parent(".pip").remove();
+            $('#prodimg' + i).click(function () { (this).remove(); });
         });
     }
 }
